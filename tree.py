6abe444fed301e8af2fe88ff9e959cbde0df6b8b
julia-richards <julia.m.richards@gmail.com>
2020-12-03 16:03:14 -0700

    adds bfs search to Node class

--- tree.py
+++ tree.py
@@ -37,9 +37,6 @@ class Node:
             node.parent = None
 
     def depth_search(self, value):
-        # visited = set()
-        # if node not in visited:
-        #     visited.add(node)
         if self._value == value:
             return self
         for child in self._children:
@@ -48,17 +45,24 @@ class Node:
                 return node
         return None
 
+    def breadth_search(self, value):
+        queue = list()
+        while queue:
+            node = queue.pop(0)
+            if node._value == value:
+                return node
+            queue.extend(node._children)
+        return None
 
 
 
 
+# node1 = Node("root1")
+# node2 = Node("root2")
+# node3 = Node("root3")
 
-node1 = Node("root1")
-node2 = Node("root2")
-node3 = Node("root3")
+# node3.parent = node1
+# node3.parent = node2
 
-node3.parent = node1
-node3.parent = node2
-
-print(node1.children)
-print(node2.children)
+# print(node1.children)
+# print(node2.children)
