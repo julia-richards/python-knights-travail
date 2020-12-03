33b15898b709c35f3248c76316ab8d86c1d025d3
julia-richards <julia.m.richards@gmail.com>
2020-12-03 14:55:52 -0700

    alters parent setter to account for edge cases"

--- tree.py
+++ tree.py
@@ -1,7 +1,7 @@
 class Node:
     def __init__(self, value):
         self._value = value
-        self._parent = None 
+        self._parent = None
         self._children = []
 
     @property
@@ -11,23 +11,38 @@ class Node:
     @property
     def children(self):
         return self._children
-    
+
     @property
     def parent(self):
         return self._parent
-    
-    
+
+    @parent.setter
+    def parent(self, node):
+        if self._parent == node:
+            return
+        if self._parent is not None:
+            self._parent.remove_child(self)
+        self._parent = node
+        if node is not None:
+            node.add_child(self)
+
     def add_child(self, node):
         if node not in self._children:
             self._children.append(node)
-            node._parent = self
+            node.parent = self
 
     def remove_child(self, node):
         if node in self._children:
             self._children.remove(node)
-            node._parent = None
+            node.parent = None
 
-    @parent.setter
-    def parent(self, node):
-        self._parent = node
-        node.add_child(self)
+
+node1 = Node("root1")
+node2 = Node("root2")
+node3 = Node("root3")
+
+node3.parent = node1
+node3.parent = node2
+
+print(node1.children)
+print(node2.children)
