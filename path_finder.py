51cabb78dcd2c75c5030c5a1ac4d85cc0226bc5e
julia-richards <julia.m.richards@gmail.com>
2020-12-03 18:53:10 -0700

    adds logic to valid moves method

--- path_finder.py
+++ path_finder.py
@@ -11,8 +11,26 @@ class KnighPathFinder:
         self._considered_positions = set(start_position)
 
     def get_valid_moves(self, pos):
-        possible_moves = list(
+        possible_moves_example = list(
             (1, 2),
             (2, 1),
-            (1, 2),
+            (-1, 2),
+            (-2, 1),
+            (2, -1),
+            (-1, -2),
+            (-2, -1),
+            (1, -2)
         )
+
+
+
+        moves = list ()
+        current_x, current_y = pos
+        for x_move, y_move in possible_moves:
+            new_set_x, new_set_y = (current_x + x_move, current_y + y_move)
+            if new_set_x in range(0, 9) and new_set_y in range(0,9):# keep from going off the board, cannot exceed 8 (everything will be b/w 0 & 8)
+            new_pos = new_set_x, new_set_y
+            moves.append(new_pos)
+        return moves
+
+    def new_move_positions(self, pos=(x, y))
