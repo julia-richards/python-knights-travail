ce1a9768307651e6ffd6e14b06f8e1f059851c3c
julia-richards <julia.m.richards@gmail.com>
2020-12-04 14:15:55 -0700

    completes phase 2

--- path_finder.py
+++ path_finder.py
@@ -1,17 +1,16 @@
 from tree import Node
 
-finder = KnighPathFinder((0,0))
+# test = Node((2, 1))
+# print(test.value)
 
-finder.find_path((2, 1))
-finder.find_path((3, 3))
 
-class KnighPathFinder:
+class KnightPathFinder:
     def __init__(self, start_position):
         self._root = Node(start_position)
         self._considered_positions = set(start_position)
 
     def get_valid_moves(self, pos):
-        possible_moves_example = list(
+        possible_moves = [
             (1, 2),
             (2, 1),
             (-1, 2),
@@ -20,17 +19,27 @@ class KnighPathFinder:
             (-1, -2),
             (-2, -1),
             (1, -2)
-        )
+        ]
 
-
-
-        moves = list ()
+        moves = []
         current_x, current_y = pos
         for x_move, y_move in possible_moves:
             new_set_x, new_set_y = (current_x + x_move, current_y + y_move)
-            if new_set_x in range(0, 9) and new_set_y in range(0,9):# keep from going off the board, cannot exceed 8 (everything will be b/w 0 & 8)
-            new_pos = new_set_x, new_set_y
+            # keep from going off the board, cannot exceed 8
+            if new_set_x in range(0, 8) and new_set_y in range(0, 8):
+                new_pos = new_set_x, new_set_y
             moves.append(new_pos)
         return moves
 
-    def new_move_positions(self, pos=(x, y))
+    def new_move_positions(self, pos):
+        # call get valid moves with pos as param
+        # filter valid moves with the considered postions
+        # add result to considered postions
+        possible_moves = set(self.get_valid_moves(pos)).difference(self._considered_positions)
+        self._considered_positions = self._considered_positions.union(possible_moves)
+
+        return possible_moves
+
+
+finder = KnightPathFinder((0, 0))
+print(finder.new_move_positions((0, 0)))
